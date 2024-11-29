1878579197563f4af0a392160fbf57656357cabb
Marcos brandao <marcosn2xw@gmail.com>
2024-11-28 22:37:06 -0300

    Adicionando dependências do projeto e configurando vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['emoji-picker-react'],
+      output: {
+        globals: {
+          'emoji-picker-react': 'EmojiPicker'
+        }
+      }
+    }
+  }
 })
